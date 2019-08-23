48ed0f9fc30eb01b3c9a7d1e065400d529d26684
HEZ1761428840 <1761428840@qq.com>
2019-08-23 17:02:38 +0800

    1.导入mysql,pagehelper,jstl,servlet,mybatis,mail依赖 2.生成反向工程

--- shoesMall/src/main/java/com/yc/shoesMall/ShoesMallApplication.java
+++ shoesMall/src/main/java/com/yc/shoesMall/ShoesMallApplication.java
@@ -1,9 +1,13 @@
 package com.yc.shoesMall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+@Controller
+@MapperScan("com.yc")
 public class ShoesMallApplication {
 
 	public static void main(String[] args) {
