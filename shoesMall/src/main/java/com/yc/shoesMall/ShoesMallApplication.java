bec854bed9d6bb33d152fcd36b45409434886691
HEZ1761428840 <1761428840@qq.com>
2019-08-23 16:14:18 +0800

    鞋子商城SpringBoot工程开始搭建

---
+++ shoesMall/src/main/java/com/yc/shoesMall/ShoesMallApplication.java
@@ -0,0 +1,13 @@
+package com.yc.shoesMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesMallApplication.class, args);
+	}
+
+}
